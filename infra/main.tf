75f7473561d5f24b9c64b74a1cdba3ad22ac1c99
Paweł Sikora <pawel.sikora2@tesco.com>
2025-04-02 23:30:42 +0100

    terraform variable for subscription_id

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,7 @@
 provider "azurerm" {
   features {}
   use_cli = true
+  subscription_id = var.subscription_id
 }
 
 resource "azurerm_resource_group" "project_rg" {
@@ -11,4 +12,5 @@ resource "azurerm_resource_group" "project_rg" {
 data "azurerm_client_config" "current" {}
 
 
+# export TF_VAR_subscription_id=$(az account show --query id --output tsv)
 # az ad sp create-for-rbac --name "architecture-workshops-sikor-github" --role Contributor --scopes /subscriptions/ba90069d-9e46-4163-9570-1b2bd4db55d4 --sdk-auth
--- infra/variables.tf
+++ infra/variables.tf
@@ -6,3 +6,8 @@ variable "location" {
 variable "project_name" {
   description = "global name of the project"
 }
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}

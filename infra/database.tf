a9ce5a58c87d5114676a8e2fe9f6e997b684e15f
Paweł Sikora <pawel.sikora2@tesco.com>
2025-04-09 22:59:50 +0200

    Added fw rule to db

--- infra/database.tf
+++ infra/database.tf
@@ -34,3 +34,10 @@ resource "azurerm_postgresql_flexible_server_database" "events_db_instance" {
   charset   = "UTF8"
   collation = "en_US.utf8"
 }
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_azure_services" {
+  name                = "AllowAzureServices"
+  server_id = azurerm_postgresql_flexible_server.events_db.id
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}

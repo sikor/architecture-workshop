b6d6c6c1eb2345f1ccf385c120ef014f97fc1364
Paweł Sikora <pawel.sikora2@tesco.com>
2025-04-10 20:58:35 +0200

    json logs formatting

--- events/build.gradle.kts
+++ events/build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 

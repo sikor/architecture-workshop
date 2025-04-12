2c33611860c94094c464bdfec8965d9490a5e3a1
Paweł Sikora <pawel.sikora2@tesco.com>
2025-04-12 20:09:21 +0200

    Added logging via swagger-ui

--- events/src/main/java/com/archiwork/events/SecurityConfig.java
+++ events/src/main/java/com/archiwork/events/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.archiwork.events;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +13,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${TENANT_ID}")
+    private String tenantId;
+
+    @Value("${AD_CLIENT_ID}")
+    private String clientId;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -23,5 +33,24 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public OpenAPI openApiWithAzureAdSecurity() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("AzureAD",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.OAUTH2)
+                                        .flows(new OAuthFlows()
+                                                .authorizationCode(new OAuthFlow()
+                                                        .authorizationUrl("https://login.microsoftonline.com/" + tenantId + "/oauth2/v2.0/authorize")
+                                                        .tokenUrl("https://login.microsoftonline.com/" + tenantId + "/oauth2/v2.0/token")
+                                                        .scopes(new Scopes().addString("api://" + clientId + "/.default", "Access API"))
+                                                )
+                                        )
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("AzureAD"));
     }
 }

c11f85d930f5d7d283ffea2f7240e0733efa8c93
Paweł Sikora <pawel.sikora2@tesco.com>
2025-05-14 20:11:19 +0200

    terraform fix

--- infra/modules/key_vault/main.tf
+++ infra/modules/key_vault/main.tf
@@ -58,9 +58,9 @@ resource "azurerm_key_vault_access_policy" "portal_access" {
   tenant_id    = var.tenant_id
   object_id    = var.portal_access_object_id
 
-  secret_permissions = ["Get", "List", "Set"]
-  key_permissions    = ["Get", "List", "Set"]
-  certificate_permissions = ["Get", "List", "Set"]
+  secret_permissions = ["Get", "List", "Update"]
+  key_permissions    = ["Get", "List", "Update"]
+  certificate_permissions = ["Get", "List", "Update"]
 }
 
 output "key_vault_id" {

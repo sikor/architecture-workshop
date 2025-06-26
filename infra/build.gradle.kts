6d756e243a2cfad90b6374b8c668641f5e870f18
Pawel Sikora <siegfriedek@gmail.com>
2025-06-26 22:09:56 +0200

    writeTerraformOutputs task simplification

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -1,7 +1,6 @@
-import groovy.json.JsonOutput
 import org.ysb33r.gradle.terraform.backends.GenericBackend
-import org.ysb33r.gradle.terraform.internal.TerraformModel
 import org.ysb33r.gradle.terraform.tasks.TerraformInit
+import org.ysb33r.gradle.terraform.tasks.TerraformOutputJson
 
 plugins {
     id("org.ysb33r.terraform")
@@ -35,30 +34,13 @@ tasks.named<TerraformInit>("tfInit") {
     backendConfigFile = property
 }
 
-val sourceSet = terraform.sourceSets.getByName(TerraformModel.DEFAULT_SOURCESET_NAME)
-val tfOutputs: Provider<Map<String, Any>> = sourceSet.rawOutputVariables()
+val tfOutputsFile: Provider<RegularFile> = tasks.named<TerraformOutputJson>("tfCacheOutputVariables").flatMap { it.statusReportOutputFile }
 
 val writeTerraformOutputs = tasks.register("writeTerraformOutputs") {
     group = "infrastructure"
     description = "Exports Terraform outputs to a JSON file"
     dependsOn("tfCacheOutputVariables")
-
-
-    val outputVariables = objects.mapProperty(String::class, Any::class)
-
-    outputVariables.set(tfOutputs)
-
-    val outputFile = layout.buildDirectory.file("terraform/outputs.json")
-    outputs.file(outputFile)
-
-    doLast {
-        val json = JsonOutput.toJson(outputVariables.get())
-        println(json)
-        outputFile.get().asFile.apply {
-            parentFile.mkdirs()
-            writeText(json)
-        }
-    }
+    outputs.file(tfOutputsFile)
 }
 
 artifacts {
